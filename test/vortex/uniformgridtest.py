612eb1e57ce47f59727f4144e9e629796af81e21
Lucas Lindström <ztargaser@gmail.com>
2015-05-15 12:06:37 +0200

    Added interpolation tests.

--- test/vortex/uniformgridtest.py
+++ test/vortex/uniformgridtest.py
@@ -101,6 +101,50 @@ class UniformGridTest(unittest.TestCase):
                 self.assertTrue(np.allclose(0, grid.contents[offset]))
 
 
+    def test_interpolate2x2(self):
+        # GIVEN
+        grid = UniformGrid(1, np.zeros(3), np.ones(3), False)
+        grid.initialize(0)
+        grid.insert(np.ones(3), 1)
+
+        # THEN
+        for coord in itertools.product(range(grid.numPoints[0]), range(grid.numPoints[1]), range(grid.numPoints[2])):
+            npCoord = np.array(coord)
+            if coord == (1, 1, 1):
+                self.assertTrue(np.allclose(1, grid.interpolate(npCoord)))
+            else:
+                self.assertTrue(np.allclose(0, grid.interpolate(npCoord)))
+
+        self.assertTrue(np.allclose(0.5, grid.interpolate(np.array([0.5, 1, 1]))))
+        self.assertTrue(np.allclose(0.5, grid.interpolate(np.array([1, 0.5, 1]))))
+        self.assertTrue(np.allclose(0.5, grid.interpolate(np.array([1, 1, 0.5]))))
+        self.assertTrue(np.allclose(0.25, grid.interpolate(np.array([0.5, 0.5, 1]))))
+        self.assertTrue(np.allclose(0.25, grid.interpolate(np.array([0.5, 1, 0.5]))))
+        self.assertTrue(np.allclose(0.25, grid.interpolate(np.array([1, 0.5, 0.5]))))
+        self.assertTrue(np.allclose(0.125, grid.interpolate(np.array([0.5, 0.5, 0.5]))))
+
+    def test_interpolate3x3(self):
+        # GIVEN
+        grid = UniformGrid(8, np.zeros(3), 2*np.ones(3), False)
+        grid.initialize(0)
+        grid.insert(np.ones(3), 1)
+
+        # THEN
+        for coord in itertools.product(range(grid.numPoints[0]), range(grid.numPoints[1]), range(grid.numPoints[2])):
+            npCoord = np.array(coord)
+            if coord == (1, 1, 1):
+                self.assertTrue(np.allclose(1, grid.interpolate(npCoord)))
+            else:
+                self.assertTrue(np.allclose(0, grid.interpolate(npCoord)))
+
+        self.assertTrue(np.allclose(0.5, grid.interpolate(np.array([0.5, 1, 1]))))
+        self.assertTrue(np.allclose(0.5, grid.interpolate(np.array([1, 0.5, 1]))))
+        self.assertTrue(np.allclose(0.5, grid.interpolate(np.array([1, 1, 0.5]))))
+        self.assertTrue(np.allclose(0.25, grid.interpolate(np.array([0.5, 0.5, 1]))))
+        self.assertTrue(np.allclose(0.25, grid.interpolate(np.array([0.5, 1, 0.5]))))
+        self.assertTrue(np.allclose(0.25, grid.interpolate(np.array([1, 0.5, 0.5]))))
+        self.assertTrue(np.allclose(0.125, grid.interpolate(np.array([0.5, 0.5, 0.5]))))
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     unittest.main()
